bd61863e87930ade3d2cf5cdbf19b5c1a09d62e7
Aditya Wiraha <squalvj@gmail.com>
2021-01-25 10:06:48 +0700

    add home component login

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,21 @@
+import { TextField, Button } from '@material-ui/core';
 export default function Home() {
   return (
-    <div>
-      asu
+    <div className="container mx-auto">
+      <div className="flex items-center h-screen">
+        <div className="w-3/6 mx-auto bg-white shadow-xl rounded flex items-center px-8 py-6 flex-col space-y-6">
+          <h1 className="text-xl">LOGIN FORM</h1>
+          <div className="w-full">
+            <TextField fullWidth label="Username"/>
+          </div>
+          <div className="w-full">
+            <TextField fullWidth type="password" label="Password"/>
+          </div>
+          <div className="w-full">
+           <Button fullWidth variant="contained" color="primary" size="large"> SUBMIT </Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
